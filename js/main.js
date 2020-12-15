3982b05cb90a07ec8e7b28c3a32e437c140a78a4
Roberta2020 <74533119+Roberta2020@users.noreply.github.com>
2020-12-15 12:36:07 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import { suma } from './js/suma.js';
+import { atimtis } from './js/atimtis.js';
+import { daugyba } from './js/daugyba.js';
+import { dalyba } from './js/dalyba.js';
+import { vidurkis } from './js/vidurkis.js';
+
+const ats1 = suma(5, 8);
+console.log(ats1);
+
+const ats2 = atimtis(5, 8);
+console.log(ats2);
+
+const ats3 = daugyba(5, 8);
+console.log(ats3);
+
+const ats4 = dalyba(5, 8);
+console.log(ats4);
+
+const ats5 = vidurkis([10, 2, 8, 4]);
+console.log(ats5);
